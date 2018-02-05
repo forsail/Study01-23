974afbcbb83fec54dab4daa994b44ff98cd2f295
lihong <lihong_sail@163.com>
2018-02-05 16:34:27 +0800

    custom view study

--- app/src/main/java/com/lee/asher/study01_23/MainActivity.java
+++ app/src/main/java/com/lee/asher/study01_23/MainActivity.java
@@ -3,11 +3,13 @@ package com.lee.asher.study01_23;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,71 +39,71 @@ public class MainActivity extends AppCompatActivity {
 //        });
 //        Test test = new Test();
 
-        final SandClockView sandClockView = (SandClockView) findViewById(R.id.custom);
-        final Button btnX = (Button) findViewById(R.id.x);
-        final Button btnY = (Button) findViewById(R.id.y);
-        final Button btnSpace = (Button) findViewById(R.id.space);
-        final Button btnStatus = (Button) findViewById(R.id.status);
-        btnX.setText("x:" + defaultX);
-        btnY.setText("y:" + defaultY);
-        btnSpace.setText("space:" + space);
-        btnX.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isPlus) {
-                    defaultX++;
-                } else {
-                    defaultX--;
-                }
-
-                btnX.setText("x:" + defaultX);
-                sandClockView.setControlX(defaultX);
-                Log.d(TAG, "onClick:x " + defaultX);
-
-            }
-        });
-
-        btnY.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isPlus) {
-                    defaultY++;
-                } else {
-                    defaultY--;
-                }
-
-                btnY.setText("y:" + defaultY);
-                sandClockView.setControlY(defaultY);
-                Log.d(TAG, "onClick:y " + defaultY);
-            }
-        });
-
-        btnSpace.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isPlus) {
-                    space++;
-                } else {
-                    space--;
-                }
-
-                btnSpace.setText("space:" + space);
-                sandClockView.setSpace(space);
-                Log.d(TAG, "onClick:space " + space);
-            }
-        });
-
-        btnStatus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isPlus = !isPlus;
-                if (isPlus) {
-                    btnStatus.setText("+");
-                } else {
-                    btnStatus.setText("-");
-                }
-            }
-        });
+//        final SandClockView sandClockView = (SandClockView) findViewById(R.id.custom);
+//        final Button btnX = (Button) findViewById(R.id.x);
+//        final Button btnY = (Button) findViewById(R.id.y);
+//        final Button btnSpace = (Button) findViewById(R.id.space);
+//        final Button btnStatus = (Button) findViewById(R.id.status);
+//        btnX.setText("x:" + defaultX);
+//        btnY.setText("y:" + defaultY);
+//        btnSpace.setText("space:" + space);
+//        btnX.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (isPlus) {
+//                    defaultX++;
+//                } else {
+//                    defaultX--;
+//                }
+//
+//                btnX.setText("x:" + defaultX);
+//                sandClockView.setControlX(defaultX);
+//                Log.d(TAG, "onClick:x " + defaultX);
+//
+//            }
+//        });
+//
+//        btnY.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (isPlus) {
+//                    defaultY++;
+//                } else {
+//                    defaultY--;
+//                }
+//
+//                btnY.setText("y:" + defaultY);
+//                sandClockView.setControlY(defaultY);
+//                Log.d(TAG, "onClick:y " + defaultY);
+//            }
+//        });
+//
+//        btnSpace.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (isPlus) {
+//                    space++;
+//                } else {
+//                    space--;
+//                }
+//
+//                btnSpace.setText("space:" + space);
+//                sandClockView.setSpace(space);
+//                Log.d(TAG, "onClick:space " + space);
+//            }
+//        });
+//
+//        btnStatus.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                isPlus = !isPlus;
+//                if (isPlus) {
+//                    btnStatus.setText("+");
+//                } else {
+//                    btnStatus.setText("-");
+//                }
+//            }
+//        });
     }
 
     class Test {
--- app/src/main/java/com/lee/asher/study01_23/hencoder/code3/CustomView3.java
+++ app/src/main/java/com/lee/asher/study01_23/hencoder/code3/CustomView3.java
@@ -2,6 +2,11 @@ package com.lee.asher.study01_23.hencoder.code3;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,12 +19,27 @@ import android.view.View;
 public class CustomView3 extends View {
     private static final String TAG = "CustomView3";
 
+    private Path path;
+    private Paint paint;
+
     public CustomView3(Context context) {
         super(context);
     }
 
     public CustomView3(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        path = new Path();
+        path.addCircle(200, 200, 100, Path.Direction.CW);
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.parseColor("#333333"));
+        paint.setTextSize(20);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
@@ -32,6 +52,16 @@ public class CustomView3 extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Log.d(TAG, "onDraw: ");
+        canvas.drawPath(path, paint);
+        canvas.drawTextOnPath("小姑凉", path, 0, 0, paint);
+
+        String str = "李宏是个小达人";
+        canvas.drawText(str, 500, 200, paint);
+        Rect rect = new Rect();
+        paint.getTextBounds(str, 0, 7, rect);
+        Log.d(TAG, "onDraw:getTextBounds " + rect.width());
+        Log.d(TAG, "onDraw:measureText " + paint.measureText(str));
+        canvas.drawLine(500, 200, 500 + paint.measureText(str), 200, paint);
     }
 
     /**
